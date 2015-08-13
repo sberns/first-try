b527d746dce4005f0db225f2535ba5d32984d162
sberns <stefan@bernsmueller.de>
2015-08-13 22:42:33 +0200

    Erste Änderung
    Meine erste Änderung

--- HelloWorld/src/de/bms/utilities/HelloWorld.java
+++ HelloWorld/src/de/bms/utilities/HelloWorld.java
@@ -15,7 +15,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");        
+        System.out.println("Mein erstes Git-Projekt!");
         // TODO code application logic here
     }
     
